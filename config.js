8e91c8f257e1f002aac3a679d8d434a026c561ee
force <107786797+force-info@users.noreply.github.com>
2022-08-10 14:19:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: '',
+  token: "MTAwMTk1NTM3MTM5ODczMzk1NQ.GJ9uQW.UzHBLpU1yhDa2mi6z6W5_DNcPgTJxJ-0wbDRTg",
   color: '#303136',
   globalBot: true, // false => if bot is private bot - true => if bot isn't private bot
   guildID: "Server ID",
